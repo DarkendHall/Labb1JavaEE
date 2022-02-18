e35e281e0ae7d654d412d7ede647b7e74f6001ae
DarkendHall <marcus.leeman@iths.se>
2022-02-18 11:11:38 +0100

    Add methods: add and get + tests

--- src/main/java/org/darkend/service/StudentService.java
+++ src/main/java/org/darkend/service/StudentService.java
@@ -1,7 +1,28 @@
 package org.darkend.service;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.darkend.entity.Student;
+
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public StudentService() {
+    }
+
+    public StudentService(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Student add(Student student) {
+        entityManager.persist(student);
+        return student;
+    }
+
+    public Student get(long Id) {
+        return entityManager.find(Student.class, Id);
+    }
 }
---
+++ src/test/java/org/darkend/service/StudentServiceTest.java
@@ -0,0 +1,39 @@
+package org.darkend.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.darkend.entity.Student;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StudentServiceTest {
+
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    StudentService studentService;
+    Student student;
+
+    @BeforeEach
+    private void setup() {
+        studentService = new StudentService(entityManager);
+        student = new Student("Marcus", "Leeman", "test@email.com");
+    }
+
+    @Test
+    @DisplayName("Add should add")
+    void add() {
+
+        var result = studentService.add(student);
+
+        var result1 = studentService.get(1);
+
+        assertThat(result).isEqualTo(student);
+        assertThat(result1).isEqualTo(student);
+    }
+
+}
