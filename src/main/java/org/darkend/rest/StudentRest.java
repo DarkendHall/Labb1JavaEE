6461a0e878457a80863de033c518645ae747bd0a
DarkendHall <marcus.leeman@iths.se>
2022-02-21 21:57:00 +0100

    Add method: updateStudent(Student student)

--- src/main/java/org/darkend/rest/StudentRest.java
+++ src/main/java/org/darkend/rest/StudentRest.java
@@ -9,6 +9,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -59,6 +60,16 @@ public class StudentRest {
     public Response updateStudent(@PathParam("id") Long id, Student student) {
 
         studentService.patch(id, student);
+
+        return Response.accepted(student)
+                .build();
+    }
+
+    @Path("")
+    @PUT
+    public Response updateStudent(Student student) {
+
+        studentService.update(student);
 
         return Response.accepted(student)
                 .build();
