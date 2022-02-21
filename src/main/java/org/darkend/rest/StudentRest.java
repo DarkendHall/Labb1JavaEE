c850ab9962ae1f81e64624a35e5f1e17678ee3f5
DarkendHall <marcus.leeman@iths.se>
2022-02-21 10:48:03 +0100

    Add method: getStudent(Long id)

--- src/main/java/org/darkend/rest/StudentRest.java
+++ src/main/java/org/darkend/rest/StudentRest.java
@@ -4,9 +4,12 @@ import org.darkend.entity.Student;
 import org.darkend.service.StudentService;
 
 import javax.inject.Inject;
+import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -36,6 +39,18 @@ public class StudentRest {
 
         return Response.created(URI.create("/students/" + student.getId()))
                 .entity(student)
+                .build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+
+        Student foundStudent = studentService.get(id);
+
+        if (foundStudent == null)
+            throw new EntityNotFoundException("Couldn't find entity in database");
+        return Response.ok(foundStudent)
                 .build();
     }
 }
