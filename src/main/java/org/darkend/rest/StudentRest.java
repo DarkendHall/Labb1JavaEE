bde798b0e2299f2fa3d416920c77bc0656934578
DarkendHall <marcus.leeman@iths.se>
2022-02-21 13:32:58 +0100

    Remove try/catch from getStudent(Long id)

--- src/main/java/org/darkend/rest/StudentRest.java
+++ src/main/java/org/darkend/rest/StudentRest.java
@@ -4,7 +4,6 @@ import org.darkend.entity.Student;
 import org.darkend.service.StudentService;
 
 import javax.inject.Inject;
-import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PATCH;
@@ -46,11 +45,7 @@ public class StudentRest {
     @Path("{id}")
     @GET
     public Response getStudent(@PathParam("id") Long id) {
-
         Student foundStudent = studentService.get(id);
-
-        if (foundStudent == null)
-            throw new EntityNotFoundException("Couldn't find entity in database");
         return Response.ok(foundStudent)
                 .build();
     }
